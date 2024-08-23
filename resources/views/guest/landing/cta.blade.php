6b325671b89c18cbd4da20438e88f38ea28c22ad
Janau00 <jana.abdullah.u00@gmail.com>
2024-08-23 21:38:40 +0300

    remove the (START NOW) button

--- resources/views/guest/landing/cta.blade.php
+++ resources/views/guest/landing/cta.blade.php
@@ -1,15 +1,15 @@
 <div class="w-[60rem] mx-auto pt-[8rem]">
     <h1 class="text-white text-center font-bold text-[72px] leading-tight">
-        Get Started with
+        
         <span class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
-            BentoLio
+            
         </span>
-        Today Unleash Your Creativity and Build Your Modern
+        
         <span class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
-            Portfolio!
+            
         </span>
     </h1>
-    <div class="flex justify-center pt-5">
+   <!-- <div  class="flex justify-center pt-5">
         @if (Route::has('register'))
         <a href="{{ route('login') }}">
             <button class="btn btn-active h-[5rem] w-[30rem] space-x-0 btn-neutral flex flex-col">
@@ -17,5 +17,5 @@
             </button>
         </a>
         @endif
-    </div>
-</div>
+    </div> -->
+</div> 
