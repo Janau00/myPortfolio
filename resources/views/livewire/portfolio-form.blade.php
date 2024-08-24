25e4bdfc6669736d32c25ae33110c43e3745f793
Janau00 <jana.abdullah.u00@gmail.com>
2024-08-24 19:31:09 +0300

    resumee

--- resources/views/livewire/portfolio-form.blade.php
+++ resources/views/livewire/portfolio-form.blade.php
@@ -59,7 +59,7 @@
                     <path d="M0 0h512v512H0z" fill="#000"/>
                     <path clip-rule="evenodd" d="M192.034 98H83l129.275 170.757L91.27 412h55.908l91.521-108.34 81.267 107.343H429L295.968 235.284l.236.303L410.746 99.994h-55.908l-85.062 100.694zm-48.849 29.905h33.944l191.686 253.193h-33.944z" fill="#fff" fill-rule="evenodd"/>
                 </svg>
-                <input wire:model="twitter" type="text" placeholder="x.com/ByteJana" class="input input-bordered bg-[#161B22] border-gray-500 focus:border-white text-white font-medium w-full max-w-xs pl-10"/>
+                <input wire:model="twitter" type="text" placeholder="x.com/cj_sagnsn" class="input input-bordered bg-[#161B22] border-gray-500 focus:border-white text-white font-medium w-full max-w-xs pl-10"/>
             </div>
         </div>
         <div class="pt-5">
